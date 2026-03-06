44659091a5fd23007b793a4adde56ce495c7e34b
Breno Guardia <brenoguardia02@outlook.com>
2026-03-06 19:50:30 -0300

    refactor: extract services data from `section-services.astro` into a new `service.mock.ts` file.

---
+++ src/mock/service.mock.ts
@@ -0,0 +1,45 @@
+export const servicesMock = [
+  {
+    icon: '🧠',
+    title: 'Terapia Individual',
+    description: 'Acompanhamento personalizado para suas necessidades.',
+  },
+  {
+    icon: '💼',
+    title: 'Orientação profissional',
+    description: 'Escolhas claras e direcionadas, para o seu sucesso.',
+  },
+  {
+    icon: '🌿',
+    title: 'Apoio à Ansiedade',
+    description: 'Estratégias para manejo da ansiedade e do estresse.',
+  },
+  {
+    icon: '✨',
+    title: 'Autoconhecimento',
+    description: 'Exploração de padrões e construção de novos caminhos.',
+  },
+  {
+    icon: '🌈',
+    title: 'Psicoterapia Infantil',
+    description:
+      'Espaço seguro para a criança expressar emoções, desenvolver autonomia e fortalecer vínculos.',
+  },
+  {
+    icon: '🌍',
+    title: 'Brasileiros no exterior',
+    description:
+      'Acolhimento psicológico para quem vive fora do país e enfrenta desafios de adaptação, identidade e pertencimento.',
+  },
+  {
+    icon: '🔎',
+    title: 'Recrutamento e seleção de pessoas',
+    description: 'Avaliação psicológica para seleção de candidatos.',
+  },
+  {
+    icon: '📝',
+    title: 'Implementação da NR-1',
+    description:
+      'Gestão de riscos psicossociais e estratégias para fortalecer a saúde mental no ambiente corporativo.',
+  },
+];
